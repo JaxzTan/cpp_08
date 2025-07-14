60f9273e07c94dbb150a3f1d4c3fd1a4ad84589a
Jaxz Tan <jaxztan@Jaxzs-MacBook-Pro.local>
2025-07-14 09:59:22 +0800

    feat: added more test case in main

--- ex00/inc/easyfind.hpp
+++ ex00/inc/easyfind.hpp
@@ -6,7 +6,7 @@
 /*   By: jaxztan <jaxztan@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/08 17:01:50 by jaxztan           #+#    #+#             */
-/*   Updated: 2025/07/08 17:04:07 by jaxztan          ###   ########.fr       */
+/*   Updated: 2025/07/14 09:30:33 by jaxztan          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,6 +15,13 @@
 #include <iostream>
 #include <exception>
 #include <algorithm>
+
+# define GREEN_H "\033[0;32m" //constructor called
+# define RESET_H "\033[0m"
+# define RED_H "\033[0;31m" //destructor called
+# define BLUE_H "\033[0;34m" // getrawbit
+# define YELLOW_H "\033[0;33m" //N/A
+# define PURPLE_H "\033[0;35m" //N/A
 
 class runtime_error : public std::exception
 {
@@ -34,3 +41,11 @@ typename T::iterator easyfind(T &container, int value)
     return it;
 }
 
+template <typename T>
+typename T::iterator easyfind(T &container, std::string value)
+{
+    typename T::iterator it = std::find(container.begin(), container.end(), value);
+    if (it == container.end())
+        throw std::runtime_error("Value not found in the container");
+    return it;
+}
--- ex00/src/main.cpp
+++ ex00/src/main.cpp
@@ -6,7 +6,7 @@
 /*   By: jaxztan <jaxztan@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/08 17:01:22 by jaxztan           #+#    #+#             */
-/*   Updated: 2025/07/08 17:16:32 by jaxztan          ###   ########.fr       */
+/*   Updated: 2025/07/14 09:43:30 by jaxztan          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,16 +14,84 @@
 
 int main(void)
 {
+    std::cout << YELLOW_H << "Testing easyfind with a vector of integers" << RESET_H << std::endl << std::endl;
     std::vector<int> vec;
-    for(int i = 10; i < 100; i += 10)
+    for(int i = 10; i <= 100; i += 10)
         vec.push_back(i);
-    std::cout << "Vector contents: " << vec.front() << std::endl;
-    try
+    // std::cout << "size of vector: " << vec.size() << std::endl;
+    try // Attempt to find the value 10 in the vector
     {
-        std::cout << "Found value: " << *easyfind(vec, 10) << std::endl;
+        std::cout << "looking for value: 10 //should found" << std::endl;
+        std::cout << BLUE_H << "Found value: " << *easyfind(vec, 10) << RESET_H << std::endl;
     }
     catch (const std::runtime_error &e) {
-        std::cerr << e.what() << std::endl;
+        std::cerr << RED_H << e.what() << RESET_H << std::endl;
+    }
+    try // Attempt to find the value 100 in the vector
+    {
+        std::cout << "looking for value: 100 //should found" << std::endl;
+        std::cout << BLUE_H << "Found value: " << *easyfind(vec, 100) << RESET_H << std::endl;
+    }
+    catch (const std::runtime_error &e) {
+        std::cerr << RED_H << e.what() << RESET_H << std::endl;
+    }
+    try // Attempt to find the value 55 in the vector
+    {
+        std::cout << "looking for value: 55 //shouldn't found" << std::endl;
+        std::cout << RED_H << "Found value: " << RESET_H << *easyfind(vec, 55) << std::endl;
+    }
+    catch (const std::runtime_error &e) {
+        std::cerr << RED_H << e.what() << RESET_H << std::endl;
+    }
+    try // Attempt to find the value 32 in the vector
+    {
+        std::cout << "looking for value: 32 //shouldn't found" << std::endl;
+        std::cout << RED_H << "Found value: " << RESET_H << *easyfind(vec, 32) << std::endl;
+    }
+    catch (const std::runtime_error &e) {
+        std::cerr << RED_H << e.what() << RESET_H << std::endl;
+    }
+
+    std::cout << "---------------------------------------------------" << std::endl;
+
+    std::vector<std::string> strVec;
+    strVec.push_back("Hello");
+    strVec.push_back("World");
+    strVec.push_back("42");
+    strVec.push_back("C++");
+    std::cout << YELLOW_H <<"Testing easyfind with a vector of strings" << RESET_H << std::endl << std::endl;
+    // std::cout << "size of vector: " << strVec.size() << std::endl;
+    try // Attempt to find the string "Hello" in the vector
+    {
+        std::cout << "looking for value: Hello //should found" << std::endl;
+        std::cout << BLUE_H << "Found value: " << *easyfind(strVec, std::string("Hello")) << RESET_H << std::endl;
+    }
+    catch (const std::runtime_error &e) {
+        std::cerr << RED_H << e.what() << RESET_H << std::endl;
+    }
+    try // Attempt to find the string "42" in the vector
+    {
+        std::cout << "looking for value: 42 //should found" << std::endl;
+        std::cout << BLUE_H << "Found value: " << *easyfind(strVec, std::string("42")) << RESET_H << std::endl;
+    }
+    catch (const std::runtime_error &e) {
+        std::cerr << RED_H << e.what() << RESET_H << std::endl;
+    }
+    try // Attempt to find the string "hello" in the vector
+    {
+        std::cout << "looking for value: hello //shouldn't found" << std::endl;
+        std::cout << RED_H << "Found value: " << RESET_H << *easyfind(strVec, std::string("hello")) << std::endl;
+    }
+    catch (const std::runtime_error &e) {
+        std::cerr << RED_H << e.what() << RESET_H << std::endl;
+    }
+    try // Attempt to find the string "43" in the vector
+    {
+        std::cout << "looking for value: 43 //shouldn't found" << std::endl;
+        std::cout << RED_H << "Found value: " << RESET_H << *easyfind(strVec, std::string("43")) << std::endl;
+    }
+    catch (const std::runtime_error &e) {
+        std::cerr << RED_H << e.what() << RESET_H << std::endl;
     }
     return 0;
 }
